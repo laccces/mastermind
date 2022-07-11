d3e0546edd60c2efc0f1b17358456d8fdf2e9816
Dan <en07djc@icloud.com>
2022-07-11 09:49:29 +0100

    Added end state and fixed loop to make game playable.

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,5 @@
 @board = []
-@computer_combo = [1, 2, 3, 4]
+@computer_combo = ["1", "2", "3", "4"]
 @i = 0
 
 #methods
@@ -17,6 +17,7 @@ def half_match_checker
   if @computer_combo.include?(@user_guess[3])
     @board << "*"
   end
+  p @board
 end
 
 def full_match_checker
@@ -32,6 +33,7 @@ def full_match_checker
   if @computer_combo[3].eql?(@user_guess[3])
     @board[3] = "!"
   end
+  p @board
 end
 
 
@@ -52,16 +54,26 @@ puts "Game started."
 while @i < 12
   puts "Please guess a 4 digit combination. Digits must be between 1 & 6."
 
+  @board = []
+
   @user_guess = gets.split(%r{\s*})
 
   puts "Your guess is:"
   p @user_guess
 
-  p half_match_checker
-  p full_match_checker
+  half_match_checker
+  full_match_checker
   break if @board == ["!", "!", "!", "!"]
-  puts "Here is your score. A * indicates a number that is in the code but not in the correct place, and a ! indicates a correct number in the correct place. This code has been shuffled, so appears in a random order. 'nil' is neither."
-  p @board.shuffle
+  puts "Here is your score. A * indicates a number that is in the code but not in the correct place, and a ! indicates a correct number in the correct place. This code has been shuffled, so appears in a random order."
+  p @board.shuffle  
   @i += 1
+end
+
+if @board == ["!", "!", "!", "!"] 
+  puts "You win!"
+end
+
+if @i == 12
+  puts "Sorry, you've run out of guesses. You lose. The computer code was #{@computer_combo}."
 end
 
